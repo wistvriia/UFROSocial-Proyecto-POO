8c5b987f675e800ff2138bf910164da13c99a55c
florencia <ne0.is.a.sh4rk@gmail.com>
2026-07-08 15:37:57 -0400

    Delete main/java/ufro/redsocial/service/impl/AlmacenamientoArchivoService.java

--- main/java/ufro/redsocial/service/impl/AlmacenamientoArchivoService.java
+++
@@ -1,7 +0,0 @@
-package cl.ufro.redsocial.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface AlmacenamientoArchivoService {
-    String guardar(MultipartFile archivo);
-}
