108ca146cf30c58ae3dac9de5220d0bb2556c66e
florencia <ne0.is.a.sh4rk@gmail.com>
2026-07-08 15:20:46 -0400

    Create RedSocialUfroApplication.java

---
+++ main/java/ufro/redsocial/util/RedSocialUfroApplication.java
@@ -0,0 +1,12 @@
+package cl.ufro.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedSocialUfroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedSocialUfroApplication.class, args);
+    }
+}
